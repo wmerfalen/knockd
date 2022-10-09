6591eaec92951208c93288ffb22b5f8da21c358f
tdadmin <admin@bnull.net>
2022-10-09 12:29:54 -0600

    refactor: remove debug

--- lib/knockd.hpp
+++ lib/knockd.hpp
@@ -9,55 +9,28 @@
 #include "xoroshiro.hpp"
 #include <cstring>
 
-#ifndef DEFAULT_TIMEOUT
-	#define DEFAULT_TIMEOUT 10
-#endif
-#ifndef CLEANUP_EVERY_N_SECONDS
-	#define CLEANUP_EVERY_N_SECONDS 20
-#endif
-#ifndef LAST_SEEN_TIMEOUT_IN_SECONDS
-	#define LAST_SEEN_TIMEOUT_IN_SECONDS 40
-#endif
-#ifndef MAX_PORTS
-	#define MAX_PORTS 64
-#endif
 #define ALLOW_COMMAND "/root/knockd-allow"
 
-#ifdef KNOCKD_SILENT
-	#ifdef KNOCKD_AGGREGATE_ERROR_STRINGS
-		#undef KNOCKD_AGGREGATE_ERROR_STRINGS
-	#endif
-	#ifdef KNOCKD_DEBUG_OUTPUT
-		#undef KNOCKD_DEBUG_OUTPUT
-	#endif
-	#ifndef KNOCKD_NO_ALERTS
-		#define KNOCKD_NO_ALERTS
-	#endif
-	#ifndef KNOCKD_NO_INFO_OUTPUT
-		#define KNOCKD_NO_INFO_OUTPUT
-	#endif
-#endif
-
-#ifdef KNOCKD_AGGREGATE_ERROR_STRINGS
-#define KNOCKD_ADD_ERROR() \
-	if(m_aggregate_strings){\
-		m_errors.emplace_back(pcap_geterr(handle));\
-	}
-#else
-#define KNOCKD_ADD_ERROR() /** - */
-#endif
 namespace libknockd {
-#ifdef KNOCKD_DEBUG_OUTPUT
-	static bool debug = true;
-	#define m_debug(A) if(debug){ std::cout << "[debug]: " << __FUNCTION__ << ":" << __LINE__ << ":->" << A << "\n"; }
-#else
-	#define m_debug(A)
-#endif
-#ifdef KNOCKD_NO_ALERTS
-	#define m_alert(A)
-#else
-	#define m_alert(A) std::cerr << "***ALERT***\n***ALERT***: " << A << "\n***ALERT***\n";
-#endif
+	struct knock {
+		in_addr ip;
+		std::size_t index;
+		time_t last_hit;
+	};
+	enum capture_result_t : int {
+		KNOCKDCAP_ERROR_DEVICE = -1,
+		KNOCKDCAP_ERROR_COMPILE = -2,
+		KNOCKDCAP_ERROR_SET_FILTER = -3,
+		SUCCESS = 0,
+	};
+	enum protocol_t : uint8_t {
+		NONE,
+		PROTO_TCP,
+		PROTO_UDP,
+	};
+	enum setup_result_t : int {
+
+	};
 	namespace util {
 		std::string ip2str(in_addr ia) {
 			char* tmp = inet_ntoa(ia);
@@ -80,34 +53,46 @@ namespace libknockd {
 		std::string to_string_limit(std::string_view str,std::size_t limit) {
 			return str.substr(0,std::min(limit,str.length())).data();
 		}
+		template <typename T,typename TStringType>
+		static inline TStringType generate_bpf_program(
+		    T* in_ports,
+		    uint8_t in_protocol
+		) {
+			TStringType local_bpf_program;
+			uint16_t local_min =  0, local_max = 0;
+			for(const auto& port : *in_ports) {
+				if(port < local_min) {
+					local_min = port;
+				}
+				if(port > local_max) {
+					local_max = port;
+				}
+			}
+			if(in_protocol == protocol_t::PROTO_TCP) {
+				local_bpf_program = "tcp";
+			} else {
+				local_bpf_program = "udp";
+			}
+			local_bpf_program += " portrange " + std::to_string(local_min);
+			local_bpf_program += "-";
+			local_bpf_program += std::to_string(local_max);
+			return local_bpf_program;
+		}
+		template <typename T, typename TFunctor>
+		static inline T& remove_from_vector(T& container, TFunctor logic) {
+			container.erase(
+			    std::remove_if(container.begin(),container.end(),logic),
+			    container.end());
+			return container;
+		}
 	};
 
 	namespace config {
-		uint16_t TIMEOUT = DEFAULT_TIMEOUT;
-		uint16_t CLEANUP_SECONDS = CLEANUP_EVERY_N_SECONDS;
-		uint16_t LAST_SEEN_TIMEOUT = LAST_SEEN_TIMEOUT_IN_SECONDS;
-		uint16_t PORT_COUNT = MAX_PORTS;
 		static constexpr uint32_t SNAPLEN_TCP = SIZE_ETHERNET + sizeof(sniff_ip) + sizeof(sniff_tcp);
 		static constexpr uint32_t SNAPLEN_UDP = SIZE_ETHERNET + sizeof(sniff_ip) + sizeof(sniff_udp);
 	};
 	namespace callback {
 		void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-	};
-	struct knock {
-		in_addr ip;
-		std::size_t index;
-		time_t last_hit;
-	};
-	enum capture_result_t : int {
-		KNOCKDCAP_ERROR_DEVICE = -1,
-		KNOCKDCAP_ERROR_COMPILE = -2,
-		KNOCKDCAP_ERROR_SET_FILTER = -3,
-		KNOCKDCAP_SUCCESS = 0,
-	};
-	enum protocol_t : uint8_t {
-		NONE,
-		PROTO_TCP,
-		PROTO_UDP,
 	};
 	struct Listener {
 			Listener() : handle(nullptr),
@@ -195,6 +180,28 @@ namespace libknockd {
 				std::fill(errbuf.begin(),errbuf.end(),0);
 				memset(&bpf_filter_program,0,sizeof(bpf_filter_program));
 			}
+			~Listener() {
+				if(handle) {
+					pcap_close(handle);
+				}
+				ports.clear();
+				handle = nullptr;
+				std::fill(errbuf.begin(),errbuf.end(),0);
+				memset(&bpf_filter_program,0,sizeof(bpf_filter_program));
+				filter_exp.clear();
+				dev.clear();
+				m_clients.clear();
+				m_allow_command.clear();
+				ip = nullptr;
+				tcp = nullptr;
+				udp = nullptr;
+				size_ip = size_tcp = 0;
+				min_port = max_port = 0;
+				max_cap = 0;
+			}
+			std::string get_error() {
+				return pcap_geterr(handle);
+			}
 			std::string_view allow_command() {
 				return m_allow_command;
 			}
@@ -205,9 +212,7 @@ namespace libknockd {
 				return m_clients;
 			}
 			knock& get_by_ip(const in_addr& src) {
-				m_debug("get_by_ip");
 				for(auto& client : m_clients) {
-					m_debug("checking");
 					if(client.ip.s_addr == src.s_addr) {
 						return client;
 					}
@@ -219,16 +224,15 @@ namespace libknockd {
 				return ref;
 			}
 			void remove_client(in_addr ip) {
-				m_clients.erase(
-				std::remove_if(m_clients.begin(),m_clients.end(),[&](const auto & param_client) {
-					return param_client.ip.s_addr == ip.s_addr;
-				}),
-				m_clients.end());
+				util::remove_from_vector<std::vector<knock>,std::function<bool(const knock&)>>(
+				m_clients,[&](const knock& client) {
+					return client.ip.s_addr == ip.s_addr;
+				}
+				    );
 			}
 			void allow_client(in_addr ip) {
 				std::string host = util::ip2str(ip);
 				if(host.length()) {
-					m_debug("Allow: " << host);
 					std::string command = m_allow_command;
 					command += " ";
 					command += host;
@@ -236,107 +240,48 @@ namespace libknockd {
 				}
 			}
 			void cleanup_clients_older_than(time_t last_seen) {
-				m_debug("running");
 				time_t now = time(nullptr);
-				m_clients.erase(
-				std::remove_if(m_clients.begin(),m_clients.end(),[&](const auto& param_client) {
+				util::remove_from_vector<std::vector<knock>,std::function<bool(const knock&)>>(
+				m_clients,[&](const auto& param_client) {
 					return now - param_client.last_hit < last_seen;
-				}),
-				m_clients.end()
-				);
-			}
-#ifdef KNOCKD_AGGREGATE_ERROR_STRINGS
-			const std::vector<std::string>& get_errors_list() const {
-				return m_errors;
-			}
-			void clear_errors_list() {
-				m_errors.clear();
-			}
-			void disable_error_string_aggregation() {
-				m_aggregate_strings = false;
-			}
-			void enable_error_string_aggregation() {
-				m_aggregate_strings = true;
-			}
-#endif
-
-#ifdef KNOCKD_NO_INFO_OUTPUT
-#define KNOCKD_INFO(A) /** - */
-#else
-			static void info(std::string_view msg) {
-				std::cout << "[info]: " << msg << "\n";
-			}
-#define KNOCKD_INFO(A) info(A)
-#endif
-
-			void dump_usage_stats() {
-#ifdef KNOCKD_NO_INFO_OUTPUT
-				std::cout << "------ [ USAGE STATS ] -------\n" <<
-				    "[ clients.size(): " << m_clients.size() << " (bytes: " << m_clients.size() * sizeof(knock) << ")]\n" <<
-				    "[--------------------------------------]\n";
-#endif
+				});
 			}
 
 			void process_packet(const struct pcap_pkthdr *header, const u_char* packet) {
-				m_debug("got_packet");
-				if(ports.size() == 0) {
-					KNOCKD_INFO("got packet, but ports.size is zero!");
-					return;
-				}
-
-				m_debug("-[ packet metadata ]-----------------------------------");
-				m_debug("packet stats: ");
-				m_debug("packet.ts      : " << header->ts.tv_sec);
-				m_debug("packet.caplen  : " << header->caplen);
-				m_debug("packet.len     : " << header->len);
-				m_debug("-------------------------------------------------------");
-
 				if(header->caplen < snaplen) {
-					KNOCKD_INFO("[info]: packet doesnt meet required minimum length");
 					return;
 				}
 
 				ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
 				size_ip = IP_HL(ip)*4;
 				if(size_ip < 20) {
-					KNOCKD_INFO("Invalid IP header length");
 					return;
 				}
-				m_debug("getting client ptr");
 				knock& client = get_by_ip(ip->ip_src);
 				client.last_hit = header->ts.tv_sec;
 
 				uint16_t port = 0;
 
 				if(protocol == protocol_t::PROTO_TCP) {
-					m_debug("is tcp");
 					tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
 					size_tcp = TH_OFF(tcp)*4;
 					if(size_tcp < 20) {
-						KNOCKD_INFO("Invalid TCP header length");
 						return;
 					}
 					port = ntohs(tcp->th_dport);
 				} else {
-					m_debug("is udp");
 					udp = (struct sniff_udp*)(packet + SIZE_ETHERNET + size_ip);
-					m_debug("getting port");
 					port = ntohs(udp->uh_dport);
-					m_debug("port :" << port);
 				}
 				std::string host = util::ip2str(ip->ip_src);
-				m_debug("IN:" << host << ":" << port);
 				if(client.index < ports.size()) {
 					if(port == ports[client.index]) {
-						m_debug("incrementing client.index for: " << host);
 						client.index++;
 					} else {
-						m_debug("Resetting for: " << host);
 						client.index = 0;
 					}
 				}
 				if(client.index >= ports.size()) {
-					m_debug("allowing client " << host);
 					allow_client(client.ip);
 					remove_client(client.ip);
 				}
@@ -347,7 +292,7 @@ namespace libknockd {
 			const int& get_max_count() const {
 				return max_cap;
 			}
-			int start_capture(
+			int run(
 			    std::string_view in_device,
 			    const protocol_t& in_protocol,
 			    const std::vector<uint16_t>& in_ports,
@@ -359,9 +304,9 @@ namespace libknockd {
 				std::fill(errbuf.begin(),errbuf.end(),0);
 				memset(&bpf_filter_program,0,sizeof(bpf_filter_program));
 				max_cap = in_max_cap;
-				return start_capture();
+				return run();
 			}
-			int start_capture(
+			int run(
 			    std::string_view in_device,
 			    const protocol_t& in_protocol,
 			    const std::vector<uint16_t>& in_ports) {
@@ -371,58 +316,62 @@ namespace libknockd {
 				snaplen = in_protocol == protocol_t::PROTO_TCP ? config::SNAPLEN_TCP : config::SNAPLEN_UDP;
 				std::fill(errbuf.begin(),errbuf.end(),0);
 				memset(&bpf_filter_program,0,sizeof(bpf_filter_program));
-				return start_capture();
+				return run();
 			}
-			int start_capture() {
-				m_debug("setup_interface()");
-				min_port = max_port = 0;
-				for(const auto& port : ports) {
-					if(port < min_port) {
-						min_port = port;
-					}
-					if(port > max_port) {
-						max_port = port;
-					}
+			std::pair<uint16_t,uint16_t> fetch_device_netmask() {
+				return fetch_device_netmask(dev);
+			}
+			std::pair<uint16_t,uint16_t> fetch_device_netmask(std::string_view dev) {
+				uint32_t local_net = 0,local_mask = 0;
+				std::fill(errbuf.begin(),errbuf.end(),0);
+				if(pcap_lookupnet((char*)dev.data(), &local_net, &local_mask, (char*)&errbuf[0]) == -1) {
+					local_net = 0;
+					local_mask = 0;
 				}
-				if(protocol == protocol_t::PROTO_TCP) {
-					filter_exp = "tcp";
-				} else {
-					filter_exp = "udp";
-				}
-				filter_exp += " portrange " + std::to_string(min_port);
-				filter_exp += "-";
-				filter_exp += std::to_string(max_port);
-				if(pcap_lookupnet((char*)dev.c_str(), &net, &mask, (char*)&errbuf[0]) == -1) {
-					net = 0;
-					mask = 0;
-				}
-				/*
-				 *
-				 * pcap_t *pcap_open_live(
-				 * 	const char *device,
-				 * 	int snaplen,
-				 * 	int promisc,
-				 * 	int to_ms,
-				 * 	char *errbuf
-				 * 	);
-				 */
+				return {local_net,local_mask};
+			}
+			int open_device() {
+				snaplen = protocol == protocol_t::PROTO_TCP ? config::SNAPLEN_TCP : config::SNAPLEN_UDP;
+				std::fill(errbuf.begin(),errbuf.end(),0);
 				handle = pcap_open_live((char*)dev.c_str(), snaplen, 1, 1000, (char*)&errbuf[0]);
 				if(handle == NULL) {
 					return capture_result_t::KNOCKDCAP_ERROR_DEVICE;
 				}
+				return 0;
+			}
+			int compile_filter() {
+				std::pair<uint16_t,uint16_t> pair = fetch_device_netmask(dev);
+				net = pair.first;
+				mask = pair.second;
+				filter_exp = util::generate_bpf_program<std::vector<uint16_t>,std::string>(&ports,protocol);
 				if(pcap_compile(handle, &bpf_filter_program, (char*)filter_exp.c_str(), 0, net) == -1) {
-					KNOCKD_ADD_ERROR();
 					return capture_result_t::KNOCKDCAP_ERROR_COMPILE;
 				}
 				if(pcap_setfilter(handle, &bpf_filter_program) == -1) {
-					KNOCKD_ADD_ERROR();
 					return capture_result_t::KNOCKDCAP_ERROR_SET_FILTER;
 				}
-				pcap_loop(handle, max_cap, callback::got_packet, reinterpret_cast<u_char*>(this));
-				return capture_result_t::KNOCKDCAP_SUCCESS;
+				return 0;
+			}
+			int start_capture(const int& max_packets) {
+				max_cap = max_packets;
+				return start_capture();
+			}
+			int start_capture() {
+				return pcap_loop(handle, max_cap, callback::got_packet, reinterpret_cast<u_char*>(this));
 			}
 			void close() {
 				pcap_close(handle);
+			}
+			int run() {
+				int ret = open_device();
+				if(ret < 0) {
+					return ret;
+				}
+				ret = compile_filter();
+				if(ret < 0) {
+					return ret;
+				}
+				return start_capture();
 			}
 
 		private:
@@ -437,10 +386,6 @@ namespace libknockd {
 			protocol_t protocol;
 			uint32_t snaplen;
 			std::vector<knock> m_clients;
-#ifdef KNOCKD_AGGREGATE_ERROR_STRINGS
-			std::vector<std::string> m_errors;
-			bool  m_aggregate_strings;
-#endif
 			std::string m_allow_command;
 			const struct sniff_ip *ip; /* The IP header */
 			const struct sniff_tcp *tcp; /* The TCP header */
